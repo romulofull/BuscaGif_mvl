cf6bb37e64b48c77f13f09543f53ac4d06e965b0
romulofull <romulo.castro@gmail.com>
2024-10-20 13:39:13 -0500

    mas Acerca De

--- BuscaGifs.js
+++ BuscaGifs.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, FlatList, Image, ActivityIndicator, StyleSheet } from 'react-native';
+import { View, TextInput, Button, FlatList, Image, ActivityIndicator, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import axios from 'axios';
 import { REACT_APP_API_KEY } from '@env';
+import Modal from 'react-native-modal';
 
 const BuscaGifs = () => {
   const [terminoBusqueda, setTerminoBusqueda] = useState('');
   const [gifs, setGifs] = useState([]);
   const [cargando, setCargando] = useState(false);
+  const [isModalVisible, setModalVisible] = useState(false);
 
   const buscarGifs = async (termino) => {
     const url = `https://api.giphy.com/v1/gifs/search?api_key=${REACT_APP_API_KEY}&q=${termino}&limit=10`;
@@ -29,6 +31,10 @@ const BuscaGifs = () => {
     }
   };
 
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible); 
+  };
+
   return (
     <View style={styles.container}>
       <TextInput
@@ -38,6 +44,10 @@ const BuscaGifs = () => {
         placeholder="Ingresa una palabra"
       />
       <Button title="Buscar" onPress={manejarBusqueda} />
+
+      <TouchableOpacity onPress={toggleModal} style={styles.aboutButton}>
+        <Text style={styles.buttonText}>ACERCA DE</Text>
+      </TouchableOpacity>
 
       {cargando ? (
         <ActivityIndicator size="large" color="#0000ff" />
@@ -55,6 +65,16 @@ const BuscaGifs = () => {
           contentContainerStyle={styles.list}
         />
       )}
+
+      <Modal isVisible={isModalVisible} onBackdropPress={toggleModal}>
+        <View style={styles.modalContent}>
+          <Text style={styles.modalTitle}>ACERCA DE</Text>
+          <Image source={require('./assets/11.png')} style={styles.image} />
+          <TouchableOpacity onPress={toggleModal} style={styles.closeButton}>
+            <Text style={styles.closeButtonText}>Cerrar</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -81,6 +101,41 @@ const styles = StyleSheet.create({
     height: 200,
     marginVertical: 10,
     borderRadius: 5,
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 22,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalTitle: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 10,
+  },
+  closeButton: {
+    marginTop: 10,
+    backgroundColor: '#007BFF',
+    padding: 10,
+    borderRadius: 5,
+  },
+  closeButtonText: {
+    color: 'white',
+  },
+  aboutButton: {
+    marginTop: 10, 
+    backgroundColor: '#007BFF',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    alignSelf: 'flex-end', 
+  },
+  buttonText: {
+    color: 'white',
   },
 });
 
